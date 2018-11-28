15353c84df6f4c3b018be44ee6a827f89a9488b8
Maria Varga <mariavarga@Marias-MacBook-Pro.local>
2018-11-28 18:47:04 -0200

    add activity multiplier

--- calculate.py
+++ calculate.py
@@ -27,20 +27,31 @@ while True:
     
 age = int(input("Enter your age: "))
 
-sex = str(input("Enter your sex (M for Male and F for Female): "))
-
-
 while True:
     try:
         sex = str(input("Enter your sex (M for Male and F for Female): "))
         # asserting weight is in Kg
-        if sex not in ["M","F"]:
+        if sex not in ['M','F']:
             raise AssertionError('Please enter the letter M or F')
         break
     except AssertionError as e:
         print(e)
 
-maria = calories(weight, height, age, sex)
+C = Calories(weight, height, age, sex)
 
-print("Your Calories {}".format(maria.TDEE(1.5)))
+activity = float(input('''
+1.2 - Sedentary: Little or no physical activity.
+1.3 - Lightly Active: Light exercise or activity 1-3 days per week.
+1.5 - Moderately Active: Moderate exercise or activity 3-5 days per week.
+1.7 - Very Active: Hard exercise or activity 6-7 days per week.
+1.9 - Extremely Active: Hard daily exercise or activity and physical work 
+
+Enter your activity multiplier according to the table above (number between 1.2 to 1.9): '''))
+
+print('''Your daily calorie intake:
+{} for maintain
+{} for losing
+{} for gaining '''.format(round(C.TDEE(activity)), round(C.cut()), round(C.bulk())))
+
+
 
